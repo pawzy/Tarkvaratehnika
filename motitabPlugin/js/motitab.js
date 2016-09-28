69ffe2f9735c25e9dbf24fa7659cbe6c2aa651df
karmenosa <karmenosa94@gmail.com>
2016-09-28 16:38:41 +0300

    * Changed id names to classes.

--- motitabPlugin/js/motitab.js
+++ motitabPlugin/js/motitab.js
@@ -1,6 +1,6 @@
 $.getJSON('/json/quotes.json', function(list) {
     var randomQuoteIDFromList = Math.floor(Math.random() * list.length);
     var quote = list[randomQuoteIDFromList];
-    $("#quote").text(quote.quote)
-    $("#author").text("- "+quote.author)
+    $(".js-quote").text(quote.quote)
+    $(".js-author").text("- "+quote.author)
 });
